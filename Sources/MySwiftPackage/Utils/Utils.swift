86851c2bfa7e65bb187325dd6820cd2ae6a18017
merajansari <merajansari@LT-MBP-060.local>
2025-03-02 21:14:49 +0530

    changes in taking ss

--- Sources/MySwiftPackage/Utils/Utils.swift
+++ Sources/MySwiftPackage/Utils/Utils.swift
@@ -1,12 +1,92 @@
+//////
+//////  Utils.swift
+//////  new swift package
+//////
+//////  Created by merajansari on 02/03/25.
+//////
+////
+////import UIKit
+////
+////
+////class Utils {
+////    private let httpClient: HttpClient
+////    
+////    init() {
+////        self.httpClient = HttpClient()
+////    }
+////    
+////    @MainActor func screenshot(screenshotDetails: inout [String: String]) -> String? {
+////        do {
+////            let base64String = try ScreenshotController().takeScreenshot()
+////            screenshotDetails["screenshot"] = base64String
+////            return httpClient.postScreenshot(screenshotDetails: screenshotDetails)
+////        } catch {
+////            print("Error taking screenshot: \(error)")
+//////            throw LTAppError.postScreenshotError("Error posting screenshot")
+////            return nil
+////        }
+////    }
+////    
+////    func realDeviceScreenshot(realDeviceScreenshotDetails: [String: Any]) -> String? {
+////        return httpClient.postRealDeviceScreenshot(realDeviceScreenshotDetails: realDeviceScreenshotDetails)
+////    }
+////    
+////}
 //
-//  Utils.swift
-//  new swift package
 //
-//  Created by merajansari on 02/03/25.
 //
+//
+//
+//
+////
+////  Utils.swift
+////  new swift package
+////
+////  Created by merajansari on 02/03/25.
+////
+//
+//import UIKit
+//
+//
+//class Utils {
+//    private let httpClient: HttpClient
+//    
+//    init() {
+//        self.httpClient = HttpClient()
+//    }
+//    
+//    @MainActor func screenshot(screenshotDetails: inout [String: String]) -> String? {
+//        do {
+//            let base64String = try ScreenshotController().takeScreenshot()
+//            print("screenshot string: ")
+//            print(base64String)
+//            screenshotDetails["screenshot"] = base64String
+//            return httpClient.postScreenshot(screenshotDetails: screenshotDetails)
+//        } catch {
+//            print("Error taking screenshot: \(error)")
+//            return nil
+//        }
+//    }
+//    
+//    func realDeviceScreenshot(realDeviceScreenshotDetails: [String: Any]) -> String? {
+//        return httpClient.postRealDeviceScreenshot(realDeviceScreenshotDetails: realDeviceScreenshotDetails)
+//    }
+//    
+//    @MainActor private func takeScreenshot() -> UIImage? {
+//        let keyWindow = UIApplication.shared.windows.first(where: { $0.isKeyWindow })
+//        let renderer = UIGraphicsImageRenderer(size: keyWindow?.bounds.size ?? .zero)
+//        return renderer.image { _ in
+//            keyWindow?.drawHierarchy(in: keyWindow?.bounds ?? .zero, afterScreenUpdates: true)
+//        }
+//    }
+//}
+//
+
+
+
 
 import UIKit
-
+import XCTest
 
 class Utils {
     private let httpClient: HttpClient
@@ -16,25 +96,24 @@ class Utils {
     }
     
     @MainActor func screenshot(screenshotDetails: inout [String: String]) -> String? {
-        guard let screenshotImage = takeScreenshot(),
-              let imageData = screenshotImage.pngData() else {
+        do {
+            let base64String = try takeScreenshot()
+            print("screenshot string: \(base64String)")
+            screenshotDetails["screenshot"] = base64String
+            return httpClient.postScreenshot(screenshotDetails: screenshotDetails)
+        } catch {
+            print("Error taking screenshot: \(error)")
             return nil
         }
-        
-        let base64String = imageData.base64EncodedString()
-        screenshotDetails["screenshot"] = base64String
-        return httpClient.postScreenshot(screenshotDetails: screenshotDetails)
     }
     
     func realDeviceScreenshot(realDeviceScreenshotDetails: [String: Any]) -> String? {
         return httpClient.postRealDeviceScreenshot(realDeviceScreenshotDetails: realDeviceScreenshotDetails)
     }
     
-    @MainActor private func takeScreenshot() -> UIImage? {
-        let keyWindow = UIApplication.shared.windows.first(where: { $0.isKeyWindow })
-        let renderer = UIGraphicsImageRenderer(size: keyWindow?.bounds.size ?? .zero)
-        return renderer.image { _ in
-            keyWindow?.drawHierarchy(in: keyWindow?.bounds ?? .zero, afterScreenUpdates: true)
-        }
+    @MainActor private func takeScreenshot() throws -> String {
+        let screenshot = XCUIScreen.main.screenshot()
+        let imageData = screenshot.pngRepresentation
+        return imageData.base64EncodedString()
     }
 }
